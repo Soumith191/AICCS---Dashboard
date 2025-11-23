6ca3334b95d89bad43efc36b71b4a7a2ac2598ea
G Soumith <cherryguggilla123@gmail.com>
2025-11-23 11:37:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,59 @@
+# These are the tools we need
+from PIL import Image   # To open and resize pictures
+import numpy as np      # To turn pictures into numbers
+import tensorflow as tf # To talk to your TFLite robot
+import streamlit as st  # To make the web app interactive
+
+# Load the robot’s brain
+interpreter = tf.lite.Interpreter(model_path="model_quantized.tflite")
+interpreter.allocate_tensors()
+
+# Get info about what the robot expects
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# This function turns any picture into something the robot can understand
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+
+def preprocess_image(img_path):
+    # Open the picture
+    img = Image.open(img_path).convert('RGB')
+    
+    # Resize to the size the robot expects
+    img = img.resize((IMG_WIDTH, IMG_HEIGHT))
+    
+    # Turn the picture into numbers 0-255
+    img_array = np.array(img, dtype=np.uint8)
+    
+    # Add a "batch" so the robot knows there is 1 picture
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    return img_array
+
+# Streamlit part: let user upload a picture
+st.title("My AI Picture Robot")
+uploaded_file = st.file_uploader("Choose a picture...", type=["jpg","png"])
+
+if uploaded_file is not None:
+    # Preprocess the picture for the robot
+    img = preprocess_image(uploaded_file)
+    
+    # Give the picture to the robot
+    interpreter.set_tensor(input_details[0]['index'], img)
+    
+    # Make the robot think
+    interpreter.invoke()
+    
+    # Get the robot's answer
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    
+    # Show the answer
+    st.write("The robot thinks:", output_data)
+    
+    # Also show the picture
+    st.image(uploaded_file, caption="Your picture", use_column_width=True)
+
 import streamlit as st
 import numpy as np
 from PIL import Image
